f208a2397e22eb067ddb7ba0ad19c0cb1be1be78
lhy <lhy143@gmail.com>
2020-02-15 20:54:38 +0800

    Cue Clue前端項目0.0.2

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="hello">
-    {{count}}
-    测试
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">活动</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3" disabled>回顾</el-menu-item>
+          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">关于我们</a></el-menu-item>
+          <el-menu-item index="5">登录</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Header from './header/Header.vue'
 export default {
+  components: { Header },
   name: 'index',
   data () {
     return {
@@ -27,22 +51,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
+<style lang="less" scoped>
+  .el-header {
+    margin: 0;
     padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
   }
 </style>
